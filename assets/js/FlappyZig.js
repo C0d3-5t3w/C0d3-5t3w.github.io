11ade68a5bf0579cb13954c82c59c5485e1c856f
5T3W <stewy1994@gmail.com>
2025-03-20 14:17:59 -0700

    Update FlappyZig.js

--- assets/js/FlappyZig.js
+++ assets/js/FlappyZig.js
@@ -87,7 +87,7 @@ class Game {
     }
 
     setupCanvas() {
-        const aspectRatio = 9/6;
+        const aspectRatio = 2/1;
         let width = Math.min(window.innerWidth * 0.95, window.innerHeight * aspectRatio * 0.95);
         let height = width / aspectRatio;
 
