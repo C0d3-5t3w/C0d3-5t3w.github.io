5356de8e8906c8eb2494f22f470279870e03f5ec
5T3W <stewy1994@gmail.com>
2025-03-25 18:05:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ $random_quote = $quotes[array_rand($quotes)];
                 <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/1Cg54cIvfa7dz1GuYHvgAd?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>                
             </div>
             <div class="quote-of-the-day">
-                <h1>Quote of the Day:</h1>
+                <h1>Random quote:</h1>
                 <h3><p><?php echo $random_quote; ?></p></h3>
             </div>
         </div>
