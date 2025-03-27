4e1446e23c4a2a3539487f20bd968a09d6d7eecc
5T3W <stewy1994@gmail.com>
2025-03-27 07:57:08 -0700

    Update links.php

--- links.php
+++ links.php
@@ -10,8 +10,31 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
         <link rel="stylesheet" href="assets/css/main.css">
         <title>Links</title>
         <style>
+            body {
+                background-color: black;
+                color: white;
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+            }
+            h1 {
+                color: white;
+            }
+            ul {
+                list-style: none;
+                padding: 0;
+            }
+            li {
+                background: lightgray;
+                color: black;
+                margin: 10px 0;
+                padding: 15px;
+                border-radius: 5px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
         </style>
     </head>
     <body>
+        <h1>Personal links:</h1>
     </body>
 </html>
