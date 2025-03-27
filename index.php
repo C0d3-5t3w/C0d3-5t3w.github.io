87f7cdceab727eddbec4842983a97bf14917694f
5T3W <stewy1994@gmail.com>
2025-03-27 06:35:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -199,6 +199,9 @@ $random_quote = $quotes[array_rand($quotes)];
             <h2>
                 <a href="particles.html" style="color: white;">Particles</a>
             </h2>
+            <h1>
+                My music:
+            </h1>
             <div id="soundcloud-widget" class="widget-container">
                 <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1947466211&color=%23040404&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/brandon-stewart-792686080" title="5T3W 🥦" target="_blank" style="color: #cccccc; text-decoration: none;">5T3W 🥦</a> · <a href="https://soundcloud.com/brandon-stewart-792686080/fa1l1ng" title="FA1L1NG" target="_blank" style="color: #cccccc; text-decoration: none;">FA1L1NG</a></div>
             </div>
